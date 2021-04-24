f2c5e5ea1a5679c58b076dc0f2189d449522c90b
kj <codnerkj@gmail.com>
2021-04-24 01:53:29 -0400

    zxczx

--- src/pages/cockpit.js
+++ src/pages/cockpit.js
@@ -61,57 +61,89 @@ class Cockpit extends Component{
         return(
         <div className="flex flex-col space-y-8 w-1/2 m-auto">
             
-            <form onSubmit={this.handleSubmit}>
-                <div class="container">
-                    <div class="bumper bumper--left"></div>
-                    <div class="bumper bumper--right"></div>
-                    <div class="controller">
-                        <div class="controller__cord"></div>
-                        <div class="left-pad">
-                        <div class="dpad">
-                            <div class="dpad__button button--vertical">
-                            <div class="dpad__arrow arrow--top"></div>
-                            <div class="dpad__arrow arrow--bottom"></div>
+            <div >
+                <div className="container">
+                    <div className="bumper bumper--left"></div>
+                    <div className="bumper bumper--right"></div>
+                    <div className="controller">
+                        <div className="controller__cord"></div>
+                        <div className="left-pad">
+                        <div className="dpad">
+                            <div className="dpad__button button--vertical">
+                            <div className="dpad__arrow arrow--top"></div>
+                            <div className="dpad__arrow arrow--bottom"></div>
                             </div>
-                            <div class="dpad__button button--horizontal">
-                            <div class="dpad__center">
-                                <div class="dpad__circle"></div>
+                            <div className="dpad__button button--horizontal">
+                            <div className="dpad__center">
+                                <div className="dpad__circle"></div>
                             </div>
-                            <div class="dpad__arrow arrow--left"></div>
-                            <div class="dpad__arrow arrow--right"></div>
-                            </div>
-                        </div>
-                        </div>
-                        <div class="middle-pad">
-                        <div class="logo">
-                            <h1 class="logo__header">Super Nintendo</h1>
-                            <h2 class="logo__byline">Entertainment System</h2>
-                        </div>
-                        <div class="start-select">
-                            <div class="start-select__button button--start">
-                            <div class="start-select__interior"></div>
-                            </div>
-                            <div class="start-select__button button--select">
-                            <div class="start-select__interior"></div>
+                            <div className="dpad__arrow arrow--left"></div>
+                            <div className="dpad__arrow arrow--right"></div>
                             </div>
                         </div>
                         </div>
-                        <div class="right-pad">
-                        <div class="right-controls">
-                            <div class="right-controls__circle"></div>
-                            <div class="button-group button-group--top">
-                            <div class="button-group__button button--x"></div>
-                            <div class="button-group__button button--y"></div>
+                        <div className="middle-pad">
+                        <div className="logo">
+                            <h1 className="logo__header">Super Nintendo</h1>
+                            <h2 className="logo__byline">Entertainment System</h2>
+                        </div>
+                        <div className="start-select">
+                            <div className="start-select__button button--start">
+                            <div className="start-select__interior"></div>
                             </div>
-                            <div class="button-group button-group--bottom">
-                            <div class="button-group__button button--a"></div>
-                            <div class="button-group__button button--b"></div>
+                            <div className="start-select__button button--select">
+                            <div className="start-select__interior"></div>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="right-pad">
+                        <div className="right-controls">
+                            <div className="right-controls__circle"></div>
+                            <div className="button-group button-group--top">
+                            <div className="button-group__button button--x"></div>
+                            <div className="button-group__button button--y"></div>
+                            </div>
+                            <div className="button-group button-group--bottom">
+                            <div className="button-group__button button--a"></div>
+                            <div className="button-group__button button--b"></div>
                             </div>
                         </div>
                         </div>
                     </div>
                 </div> 
-            </form>
+            </div>
+            <div className="">
+                <div>
+                    <br /><br /><br /><br /><br /><br /><br /><br /><br />
+                    <br /><br /><br /><br /><br />
+                    <h1 className="text-xl">Cockpit Console:</h1>
+                    <textarea className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" rows="9"></textarea>
+                </div>
+                <div>
+                    <div className="border-4 border-blue-600">
+                        <div className="flex flex-wrap">
+
+                                <div className="w-1/4 h-20 flex items-center">
+                                    <label htmlFor=""><b> Choose a user:</b></label>
+                                </div>
+                                <div className="w-1/4 h-20 flex items-center">
+                                    <select className="p-2 rounded" name="c_usr_an_id" >
+                                    <option value="0"  >-- Pick User --</option>
+                                      
+                                    </select>
+                                </div>
+                                <div className="w-1/4 h-20 flex items-center">
+                                    <button >
+                                        test
+                                    </button> 
+                                </div>
+                                <div className="w-1/4 h-20 flex items-center">
+                                    <span ></span>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         )
     }
